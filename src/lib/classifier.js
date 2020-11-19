ecc8a3f5ee4f063b9e33c1d8fe77614d509b82e5
Zach Ellis <zachary.ellis@apricityhealth.com>
2020-11-18 19:24:24 -0600

    Outlined classifier

--- src/lib/classifier.js
+++ src/lib/classifier.js
@@ -1,8 +1,38 @@
-import * as dfd from 'danfojs-node';
-import * as tf from '@tensorflow/tfjs-node';
+import * as tf from '@tensorflow/tfjs';
+import {
+    Series,
+    DataFrame
+} from 'pandas-js';
 
+/**
+ * Tensorflow classifier
+ */
 export class Classifier {
-    constructor() {}
+    constructor() {
+    }
+
+    // pls delete
+    mcTest() {
+        let df1 = new DataFrame([{A: 1, B: 2}, {A: 3, B: 4}]);
+        console.log( df1.toString() );
+    }
+
+    /**
+     * Fit the training data to the model
+     * @param {DataFrame} X Preproccessed song data
+     * @param {DataFrame} Y Preprocessed song classifications
+     */
+    fit(X, Y) {
+
+    }
+
+    /**
+     * Classify a song
+     * @param {DataFrame} songData Song data to predict against
+     */
+    predict(songData) {
+
+    }
 }
 
 export default Classifier;
